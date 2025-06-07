6a3c43a2afe55fcf731ee9a8d9d4e0eef9cef76a
tb-software <151437619+tb-software@users.noreply.github.com>
2025-06-07 09:13:25 +0200

    Update Program.cs

--- NetProject/Program.cs
+++ NetProject/Program.cs
@@ -1,4 +1,4 @@
 using System;
 d
-class Program { static void Main() { Console.WriteLine("Hallo von Timo 3f!"); } }
+class Program { static void Main() { Console.WriteLine("Hallo von Timo 3g!"); } }
 
